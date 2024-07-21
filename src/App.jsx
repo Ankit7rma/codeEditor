b7b320be804570dd1c3afe3bfb6165f6fd6dbdb4
Ankit7rma <ankitsharma290101@gmail.com>
2024-07-21 13:59:55 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  return <>Ankit</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
